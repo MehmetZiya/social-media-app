79ef5df60a11d63a4862c0b07eec98bd0794b256
Mehmet Ziya <mehmetziya82@gmail.com>
2021-11-30 16:27:24 +0100

    created Profile,Login and Register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import Home from './pages/home/Home'
-
+//import Home from './pages/home/Home'
+//import Profile from './pages/profile/Profile'
+import Login from './pages/login/Login'
 const App = () => {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <Login />
     </div>
   )
 }
